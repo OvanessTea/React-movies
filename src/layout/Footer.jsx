9e7f4d704ff7c445a383774d6fa3bbdcd3e5a87a
Eduard Suslov <OvanessTea@yandex.com>
2021-11-30 14:49:36 +0300

    refactor with hooks

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,14 +1,16 @@
 function Footer() {
-    return(
+    return (
         <footer className="page-footer cyan lighten-5">
             <div className="footer-copuright">
                 <div className="container blue-grey-text lighten-3">
                     © {new Date().getFullYear()} Copyright Text
-                    <a className="blue-grey-text text-darken-2 right" href="#!">Repo</a>
+                    <a className="blue-grey-text text-darken-2 right" href="#!">
+                        Repo
+                    </a>
                 </div>
             </div>
         </footer>
-    )
+    );
 }
 
-export {Footer}
+export { Footer };
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -2,12 +2,24 @@ function Header() {
     return (
         <nav className="cyan lighten-4">
             <div className="nav-wrapper">
-                <a href="index.html" className="brand-logo blue-grey-text text-darken-2">React Movies</a>
+                <a
+                    href="index.html"
+                    className="brand-logo blue-grey-text text-darken-2"
+                >
+                    React Movies
+                </a>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
-                    <li><a href="index.html" className="blue-grey-text text-darken-2">Repo</a></li>
+                    <li>
+                        <a
+                            href="index.html"
+                            className="blue-grey-text text-darken-2"
+                        >
+                            Repo
+                        </a>
+                    </li>
                 </ul>
             </div>
         </nav>
-    )
+    );
 }
-export {Header};
+export { Header };
